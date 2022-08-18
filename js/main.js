9193e604835f471b392fa27a510b1fa2d2008943
Gustavo Henrique Valentim Gomes <40806552+gustavohvg@users.noreply.github.com>
2022-08-17 21:20:59 -0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+const inputDescripTodoAdd = $('#descrip-todo-add');
+const btnAddTodo = $('#btn-add');
+const listTodo = $('#list-todo');
+
+const addTodo = (str) => {
+    if (str == ''){
+        inputDescripTodoAdd.css('border-color', '#f00').focus();
+        return false;
+    } else{
+        inputDescripTodoAdd.css('border-color', '#ced4da');
+    }
+    const content = `
+    <li>
+        <span class="material-icons-outlined mark-todo" title="marcar tarefa como concluída" data-marked="false" onclick="markUnmarkTodo($(this))" tabindex="0">circle</span>
+        <span class="text-todo">${str}</span>
+        <span class="material-icons del-todo" title="remover tarefa" onclick="delTodo($(this))" tabindex="0">remove</span>
+    </li>
+    `;
+
+    inputDescripTodoAdd.val('').focus();
+    listTodo.append(content);
+};
+const markUnmarkTodo = (e) => {
+    const isMark = e.attr('data-marked') === 'true';
+
+    if (isMark)
+        e.text('circle').attr('title', 'marcar tarefa como concluída').attr('data-marked', 'false').toggleClass('material-icons-outlined material-icons');
+    else
+        e.text('check_circle').attr('title', 'marcar tarefa como não concluída').attr('data-marked', 'true').toggleClass('material-icons-outlined material-icons');
+};
+const delTodo = (e) => {
+    e.parent().remove();
+};
+
+btnAddTodo.click(() => {
+    const valDescripTodoAdd = inputDescripTodoAdd.val();
+
+    addTodo(valDescripTodoAdd);
+});
+inputDescripTodoAdd.on('keydown', (event) => {
+    if(event.keyCode === 13) {
+        btnAddTodo.click();
+    }
+});
